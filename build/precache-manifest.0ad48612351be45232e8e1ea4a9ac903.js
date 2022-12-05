20ee6b92fd0668b7567e3ac4fba05018441688ae
Iryna Lakiza <110246105+LiraFi@users.noreply.github.com>
2022-12-05 12:56:41 +0200

    build folder added

---
+++ build/precache-manifest.0ad48612351be45232e8e1ea4a9ac903.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba06f3c5a9ab4d7d570c",
+    "url": "/static/css/main.d648e389.chunk.css"
+  },
+  {
+    "revision": "ba06f3c5a9ab4d7d570c",
+    "url": "/static/js/main.ba06f3c5.chunk.js"
+  },
+  {
+    "revision": "7b2e37571c17e8d4fdd1",
+    "url": "/static/js/1.7b2e3757.chunk.js"
+  },
+  {
+    "revision": "fdec57f51b2fc0f764ac",
+    "url": "/static/css/2.e627dd55.chunk.css"
+  },
+  {
+    "revision": "fdec57f51b2fc0f764ac",
+    "url": "/static/js/2.fdec57f5.chunk.js"
+  },
+  {
+    "revision": "d28ddbbc9f44c59e39dd",
+    "url": "/static/js/runtime~main.d28ddbbc.js"
+  },
+  {
+    "revision": "136dbecc39f9bb157af46fd3d1436820",
+    "url": "/index.html"
+  }
+];
